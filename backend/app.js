e29203bec2374e7381d51eeb2a942d46e9541650
Gauthami S <gauthami.s@gadgeon.com>
2025-12-16 19:50:47 +0530

    Dec 15 2025 commit

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json())
+app.use((req, res, next) => {
+    console.log(req.method, 'request')
+    console.log('Middleware executed'); next();
+})
 app.get('/', (req, res) => {
     res.send('First GET API Call')
 })
-
+app.post('/tasks', (req, res) => {
+    console.log(req.body)
+    res.json({
+        message: 'Task received', data: req.body
+    })
+})
 app.listen(3100, () => {
-    console.log('Server is running on port 3000');
+    console.log('Server is running on port 3100');
 })
